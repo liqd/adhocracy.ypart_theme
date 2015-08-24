48f3e10aba506c2f867361ae9c34841ffdc29303
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-08-24 12:02:07 +0200

    implement video embedding

---
+++ adhocracy/ypart_theme/static/javascripts/video.js
@@ -0,0 +1,36 @@
+(function() {
+    "use strict";
+
+    var embedVideos = function(selector) {
+        var $wrapper = $(selector || document);
+
+        var embedVideo = function(morphUrl) {
+            return function(i, el) {
+                var $el = $(el);
+                var src = $el.attr('src');
+                var url = morphUrl(src);
+                var iframe = $('<iframe frameborder="0" class="embed-video" allowfullscreen>')
+                    .attr('src', url)
+                    .css('display', 'block')
+                    .css('max-width', '100%')
+                    .css('width', '560px')
+                    .css('height', '315px')
+                    .css('margin', '1em 0');
+                $el.replaceWith(iframe);
+            };
+        };
+
+        $wrapper.find('img[alt=vimeo]').each(embedVideo(function(src) {
+            var id = src.match(/.*\/(.*)$/)[1];
+            return 'https://player.vimeo.com/video/' + id;
+        }));
+        $wrapper.find('img[alt=youtube]').each(embedVideo(function(src) {
+            var id = (src.match(/.*\?v=(.*)$/) || src.match(/.*youtu\.be\/(.*)/))[1];
+            return 'https://www.youtube.com/embed/' + id;
+        }));
+    };
+
+    $(document).ready(function() {
+        embedVideos('.proposal');
+    });
+})();
